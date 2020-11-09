5468cd1d42972c1ec4defabc6adb05789b5681bc
Gabriel Rosin <39499969+gabao55@users.noreply.github.com>
2020-11-09 14:56:01 -0300

    Update lab 2.py

--- lab 2.py
+++ lab 2.py
@@ -1,19 +1,27 @@
-#Experience
+#   Calculadora:    #
+
+
+#Entrada:
 a = input('')
+b = input('')
+c = input('')
+
+#Início do Algoritmo:
 if a.isdigit():
     x = int(a)
     tipo_numero1 = 'int'
 else:
     tipo_numero1 = 'float'
     x = float(a)
-b = input('')
-c = input('')
+
 if c.isdigit():
     y = int(c)
     tipo_numero2 = 'int'
 else:
     tipo_numero2 = 'float'
     y = float(c)
+    
+#Saída:
 if b == '+':
     if tipo_numero1 == 'int' and tipo_numero2 == 'int':
         print(x+y)
