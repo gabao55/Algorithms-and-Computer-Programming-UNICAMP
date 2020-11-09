fb10227eac22f58289c8f3d02566ca4a1bc3e562
Gabriel Rosin <39499969+gabao55@users.noreply.github.com>
2020-11-09 14:53:55 -0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,7 @@
+#Entradas:
 tipodonumero1 = input('')
+
+#Início do Algoritmo:
 if tipodonumero1=='i':
     a = int(input(''))
 elif tipodonumero1=='f':
@@ -8,4 +11,6 @@ if tipodonumero2=='i':
     b = int(input(''))
 elif tipodonumero2=='f':
     b = float(input(''))
+    
+#Saída:
 print(format(a + b, '.2f')) if (tipodonumero1=='f' or tipodonumero2=='f') else print(a + b)
